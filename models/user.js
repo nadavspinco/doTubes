c6a5cf8a6e6bd36b76d86cb70ce711d9d8c67bf7
Nadav Spinco <nadavspin@gmail.com>
2021-07-24 10:08:54 +0300

    add authorization mechanism

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const validator = require('mongoose-validator')
+
+const userSchema = new Schema({
+    email:{
+        type:String,
+        required: true,
+        unique: true,
+        validate: [
+            validator({
+              validator: 'isEmail',
+              message: 'please enter valid email'
+            })
+          ],
+    },
+    password:{
+        type:String,
+        required: true,
+    },
+
+    fullName: {
+        type: String,
+        required: true
+    },
+    userType: {
+        type: String,
+        enum : ['user','admin'],
+        default: 'user',
+        required: true
+    },
+    role: {
+        type: String,
+    },
+    description : {
+        type: String,
+    }
+    
+})
+
+module.exports = mongoose.model('User',userSchema)
---
+++ utils/validation.js
@@ -0,0 +1,6 @@
+exports.validatePassword = (password) =>{
+    let passwordre = new RegExp(
+        "(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})"
+      );
+      return passwordre.test(password); 
+} 
