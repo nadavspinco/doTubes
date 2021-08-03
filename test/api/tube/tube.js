f74603a69267fc1457244235aae39fa0eb7b5492
Nadav Spinco <nadavspin@gmail.com>
2021-08-03 14:45:58 +0300

    Get Tubes
    get all tubes I joined by a teamId

--- test/api/tube/tube.js
+++ test/api/tube/tube.js
@@ -6,7 +6,7 @@ const request = require("supertest");
 const { assert, expect: chaiExpect } = require("chai");
 
 describe("tubes", () => {
-  let jwt, teamId, jwt2 ,jwt3;
+  let jwt, teamId, jwt2, jwt3, tubeId;
   before(() => {
     const uri = "mongodb://localhost:27017/doTubes";
     return mongoose.connect(uri, {
@@ -47,81 +47,116 @@ describe("tubes", () => {
           jwt2 = res.body.jwt;
         });
     });
-        before(() => {
-          return request(app)
-            .post("/auth/signup")
-            .send({
-              email: "user3@walla.com",
-              fullName: "avi c",
-              password: "123456Aa!",
-            })
-            .then((res) => {
-              jwt3 = res.body.jwt;
-            });
+    before(() => {
+      return request(app)
+        .post("/auth/signup")
+        .send({
+          email: "user3@walla.com",
+          fullName: "avi c",
+          password: "123456Aa!",
+        })
+        .then((res) => {
+          jwt3 = res.body.jwt;
         });
-        before( () => {
-          return request(app)
-            .post("/teams/")
-            .set("Authorization", "Bearer " + jwt)
-            .send({
-              teamName: "my team",
-            })
-            .then((res) => {
-              teamId = res.body.team._id;
-            });
-        });
-          before(() => {
-          return request(app)
-              .post("/teams/join")
-              .set("Authorization", "Bearer " + jwt2)
-              .send({
-                  name: "my tube",
-                  teamId: teamId,
-              })
-      });
-      it("added tube to team failed, unauthorized user", () => {
-            return request(app)
-              .post("/tubes/")
-              .set("Authorization", "Bearer " + jwt3)
-              .send({
-                name: "my tube",
-                teamId: teamId,
-              })
-              .expect(401);
-      });
-      it("added tube to team succeed", () => {
-         return request(app)
-           .post("/tubes/")
-           .set("Authorization", "Bearer " + jwt2)
-           .send({
-             name: "my tube",
-             teamId: teamId,
-           })
-           .expect(201);
-      });
-      it("added tube failed, invalid team id", () => {
-          return request(app)
-              .post("/tubes/")
-              .set("Authorization", "Bearer " + jwt2)
-              .send({
-                  name: "my tube",
-                  teamId: "abc",
-              })
-              .expect(404);
-      });
-      it("added tube failed, invalid jwt", () => {
-        return request(app)
-          .post("/tubes/")
-          .set("Authorization", "Bearer " + jwt2 + 'a')
-          .send({
-            name: "my tube",
-            teamId: "abc",
-          })
-          .expect(401);
-      });
-    
     });
-  
- 
+    before(() => {
+      return request(app)
+        .post("/teams/")
+        .set("Authorization", "Bearer " + jwt)
+        .send({
+          teamName: "my team",
+        })
+        .then((res) => {
+          teamId = res.body.team._id;
+        });
+    });
+    before(() => {
+      return request(app)
+        .post("/teams/join")
+        .set("Authorization", "Bearer " + jwt2)
+        .send({
+          name: "my tube",
+          teamId: teamId,
+        });
+    });
+    it("added tube to team failed, unauthorized user", () => {
+      return request(app)
+        .post("/tubes/")
+        .set("Authorization", "Bearer " + jwt3)
+        .send({
+          name: "my tube",
+          teamId: teamId,
+        })
+        .expect(401);
+    });
+    it("added tube to team succeed", () => {
+      return request(app)
+        .post("/tubes/")
+        .set("Authorization", "Bearer " + jwt2)
+        .send({
+          name: "my tube",
+          teamId: teamId,
+        })
+        .expect(201)
+        .then((res) => {
+          chaiExpect(res.body).to.have.property("tube");
+          tubeId = res.body.tube._id;
+        });
+    });
+    it("added tube failed, invalid team id", () => {
+      return request(app)
+        .post("/tubes/")
+        .set("Authorization", "Bearer " + jwt2)
+        .send({
+          name: "my tube",
+          teamId: "abc",
+        })
+        .expect(404);
+    });
+    it("added tube failed, invalid jwt", () => {
+      return request(app)
+        .post("/tubes/")
+        .set("Authorization", "Bearer " + jwt2 + "a")
+        .send({
+          name: "my tube",
+          teamId: "abc",
+        })
+        .expect(401);
+    });
 
+    
   });
+  describe("get tubes", () => {
+    it("get tubes ,user is part of tubes", () => {
+      return request(app)
+        .get("/tubes/all/" + teamId)
+        .set("Authorization", "Bearer " + jwt2)
+        .expect(200)
+        .then((res) => {
+          chaiExpect(res.body).to.have.property("tubes");
+          chaiExpect(res.body.tubes).to.have.lengthOf.above(0);
+        });
+    });
+
+    it("get tubes ,user is not part of any tubes", () => {
+      return request(app)
+        .get("/tubes/all/" + teamId)
+        .set("Authorization", "Bearer " + jwt)
+        .expect(200)
+        .then((res) => {
+          chaiExpect(res.body).to.have.property("tubes");
+          chaiExpect(res.body.tubes).to.have.lengthOf(0);
+        });
+    });
+
+    it("get tubes ,user is not part of team", () => {
+      return request(app)
+        .get("/tubes/all/" + teamId)
+        .set("Authorization", "Bearer " + jwt3)
+        .expect(401)
+        .then((res) => {
+          chaiExpect(res.body).to.not.have.property("tubes");
+        });
+    });
+  });
+});
