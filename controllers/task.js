af6198d20056b9e0142508391573bcaa1b7b71b0
Nadav Spinco <nadavspin@gmail.com>
2021-08-07 14:24:15 +0300

    Add task

--- controllers/task.js
+++ controllers/task.js
@@ -11,16 +11,15 @@ const { handleErrors } = require("./error.js");
 const ObjectId = require("mongodb").ObjectId;
 
 exports.addTask = async (req, res, next) => {
-    handleErrors(req, res, next, 402);
-    console.log("after handleErrors");
+    handleErrors(req, res, next, 400);
   const { score, name, tubeId, type ,user,userId} = req.body;
     const tube = await Tube.findById(tubeId);
     if (!tube) {
-        res.status(401).json({ message: "tube is not exists" });
+        res.status(404).json({ message: "tube is not exists" });
         return;
     }
-    if (!tube.users.includes(user._id)) {
-        res.status(404).json({ message: "not authorized" });
+    if (tube.admin.toString() !== user._id.toString()) {
+        res.status(401).json({ message: "not authorized" });
         return;
     }
      const task = new Task({
@@ -29,17 +28,18 @@ exports.addTask = async (req, res, next) => {
       type: type,
       tube: tube._id,
     });
-    if (userId && !tube.user.includes(new ObjectId(userId))) {
+    if (userId && !tube.users.includes(new ObjectId(userId))) {
         res.status(401).json({ message: "user is not part of the tube" });
         return;
     }
-    if(userId) {
-        task.exacutor = new ObjectId(userId);
-    }
+      task.exacutor = new ObjectId(userId);
     task.save()
         .then(result => {
             if (result) {
-                res.status(201).json({ message: "task added successfully", task: result });
+                res.status(201).json({
+                  message: "task added successfully",
+                  task: result,
+                });
             }
             else {
                 res.status(500).json({ "message:": "task not added" });
