ad2103fc5c4b8a8d9352eda42b2e37a52573ae02
Nadav Spinco <nadavspin@gmail.com>
2021-08-07 11:59:07 +0300

    Add color to tube

--- models/task.js
+++ models/task.js
@@ -1,50 +1,53 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const taskSchema = new Schema({
-  score: {
-    type: Number,
-    required: true,
-    min: 10,
-    max: 100,
+const taskSchema = new Schema(
+  {
+    score: {
+      type: Number,
+      required: true,
+      min: 10,
+      max: 100,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    dicription: {
+      type: String,
+    },
+    exacutor: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    tube: {
+      type: Schema.Types.ObjectId,
+      ref: "Tube",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["ux", "ui", "aws", "git", "fixing bugs"],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "pre-estimated", "in-process", "completed"],
+      required: true,
+      default: "pending",
+    },
+    startDateTime: {
+      type: Date,
+    },
+    endDateTime: {
+      type: Date,
+    },
+    estimatedDateTime: {
+      type: Date,
+    },
+    
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  dicription: {
-    type: String,
-  },
-  exacutor: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  tube: {
-    type: Schema.Types.ObjectId,
-    ref: "Tube",
-    required: true,
-  },
-  type: {
-    type: String,
-    enum: ["ux", "ui", "aws", "git", "fixing bugs"],
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "in-process", "completed"],
-    required: true,
-    default: "pending",
-  },
-  startDateTime: {
-    type: Date,
-  },
-  endDateTime: {
-    type: Date,
-  },
-  estimatedDateTime: {
-    type: Date,
-  },
-
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Task", taskSchema);
